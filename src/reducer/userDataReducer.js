abf2c18c403c866aa840d073cf18bb4f6883074a
Pratik Devle <pratik.devle98@gmail.com>
2022-04-24 21:31:17 +0530

    removed commented code

--- src/reducer/userDataReducer.js
+++ src/reducer/userDataReducer.js
@@ -3,17 +3,10 @@ import {USER_ACTIONS} from "./constant";
 const userDataReducer = (state, action) => {
   switch (action.type) {
     case USER_ACTIONS.INITIAL_PLAYLIST:
-    // return {...state, playlist: [...action.payload]};
     case USER_ACTIONS.ADD_NEW_PLAYLIST:
     case USER_ACTIONS.DELETE_PLAYLIST:
       return {...state, playlist: [...action.payload]};
     case USER_ACTIONS.ADD_VIDEO_TO_PLAYLIST:
-    // return {
-    //   ...state,
-    //   playlist: state.playlist.map((item) =>
-    //     item.title === action.payload.title ? action.payload : item
-    //   ),
-    // };
     case USER_ACTIONS.REMOVE_VIDEO_FROM_PLAYLIST:
       return {
         ...state,
